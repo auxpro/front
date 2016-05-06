c5809a9b7f61e9c255d27f871c6372cbc8800754
ash.ruins66 <ash.uncover@gmail.com>
2016-05-06 12:06:11 +0200

    switched to single quotes

--- src/services/rest/RestService.js
+++ src/services/rest/RestService.js
@@ -4,7 +4,7 @@ export default class RestService {
 	static _request (reqParam) {
 			 var _CONFIG = {
 				BASE_URL : 'http://127.0.0.1:8090/rest',
-				HEADER_TOKEN : "Authorization"
+				HEADER_TOKEN : 'Authorization'
 			};
 			reqParam.url = _CONFIG.BASE_URL + reqParam.url;
 			reqParam.data = reqParam.data  || {}; 
@@ -12,7 +12,7 @@ export default class RestService {
 				var xhr = new XMLHttpRequest();
 				xhr.open(reqParam.method, reqParam.url, true);
 				xhr.setRequestHeader(_CONFIG.HEADER_TOKEN, reqParam.token);
-				xhr.setRequestHeader("Content-type", "application/json");
+				xhr.setRequestHeader('Content-type', 'application/json');
 				xhr.onload = function(oEvt) {
 					if (xhr.readyState === 4) {
 						if (xhr.status === 200 || xhr.status === 201) {
